f8074c75c7683fa87dbb795169e01c1ff2fbfa2d
Oleksii Biletskyi <alenikbel@ukr.net>
2022-12-02 17:05:14 +0200

    add feedback begining

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import { FeedbackOptionsV2 } from './FeedbackOptionsV2/FeedbackOptionsV2';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FeedbackOptionsV2 />
     </div>
   );
 };
---
+++ src/components/FeedbackOptionsV2/FeedbackOptionsV2.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useState } from 'react';
+
+export const FeedbackOptionsV2 = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+};
